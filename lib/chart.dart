501dd2f2dbf3f6c5e075cdfcc640454e57ebebc1
Hanssel36 <Hanssel36@gmail.com>
2023-02-10 23:16:43 -0500

    Routes and Icons added

---
+++ lib/chart.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ChartsScreen extends StatelessWidget {
+  const ChartsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => context.go('/'),
+          icon: Icon(Icons.arrow_back),
+        ),
+      )),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hirikana/chart.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  MyApp({super.key});
+/// The route configuration.
+final GoRouter _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return HomeScreen();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'charts',
+          builder: (BuildContext context, GoRouterState state) {
+            return const ChartsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig: _router,
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class HomeScreen extends StatefulWidget {
+  HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   bool change = true;
 
   @override
@@ -19,17 +52,39 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(),
-        backgroundColor: Color.fromARGB(255, 42, 42, 42),
-        body: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: const Color.fromARGB(255, 42, 42, 42),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    change = !change;
-                  });
-                },
-                child: Text(change ? "Hiragana" : "Katakana")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        change = !change;
+                      });
+                    },
+                    child: Text(change ? "Hiragana" : "Katakana")),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: () => context.go('/charts'),
+                    iconSize: 50,
+                    icon: const Icon(
+                      Icons.bookmark_outline,
+                    )),
+                IconButton(
+                    onPressed: () {},
+                    iconSize: 50,
+                    icon: const Icon(
+                      Icons.settings_outlined,
+                    )),
+              ],
+            ),
           ],
         ),
       ),
