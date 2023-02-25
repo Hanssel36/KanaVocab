006bf6a17616ff813ada618482a1a5c0fdd10d56
Hanssel36 <Hanssel36@gmail.com>
2023-02-25 10:50:45 -0500

    small change

--- lib/chart.dart
+++ lib/chart.dart
@@ -8,14 +8,13 @@ class ChartsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double textSize = 25;
     return MaterialApp(
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
           appBar: AppBar(
-                   elevation: 0,
-              backgroundColor: backGroundDark,
+            elevation: 0,
+            backgroundColor: backGroundDark,
             bottom: const TabBar(tabs: [
               Tab(text: "Hiragana"),
               Tab(
@@ -31,13 +30,13 @@ class ChartsScreen extends StatelessWidget {
           backgroundColor: backGroundDark,
           body: TabBarView(children: [
             makeChartView(
-                "Hiragana",
+                "BASIC",
                 charData.hiraganaCharacterMap,
                 charData.hiraganaMap,
                 charData.hiraganaVariationMap,
                 charData.hiraganaCombinationMap),
             makeChartView(
-                "Katakana",
+                "BASIC",
                 charData.katakanaCharacterMap,
                 charData.katakanaMap,
                 charData.katakanaVariationMap,
@@ -99,31 +98,37 @@ ListView makeChartView(
   return ListView(
     children: [
       Text(
-          style: const TextStyle(fontSize: 30),
+          style: const TextStyle(fontSize: 20, color: Colors.white),
           textAlign: TextAlign.center,
           type),
+      const Divider(
+        thickness: 0.5,
+        indent: 20,
+        endIndent: 20,
+        color: Colors.white30,
+      ),
       makeChart(typeMap, charMap),
       const Text(
-          style: TextStyle(fontSize: 30),
+          style: TextStyle(fontSize: 20, color: Colors.white),
           textAlign: TextAlign.center,
           "VARIATIONS"),
-                      const Divider(
-              thickness: 0.5,
-              indent: 20,
-              endIndent: 20,
-              color: Colors.white30,
-            ),
+      const Divider(
+        thickness: 0.5,
+        indent: 20,
+        endIndent: 20,
+        color: Colors.white30,
+      ),
       makeChart(typeVariations, charMap),
       const Text(
-          style: TextStyle(fontSize: 30),
+          style: TextStyle(fontSize: 20, color: Colors.white),
           textAlign: TextAlign.center,
           "COMBINATIONS"),
-                      const Divider(
-              thickness: 0.5,
-              indent: 20,
-              endIndent: 20,
-              color: Colors.white30,
-            ),
+      const Divider(
+        thickness: 0.5,
+        indent: 20,
+        endIndent: 20,
+        color: Colors.white30,
+      ),
       makeChart(typeCombinations, charMap),
 
       //makeChart(charData.hiraganaCharacterMap),
