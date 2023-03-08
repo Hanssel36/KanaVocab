1625fcffbc24f5329e825b82ee3f24e0b0495733
Hanssel36 <Hanssel36@gmail.com>
2023-03-07 19:34:49 -0500

    Incorrect feedback

--- lib/practice/keyboard.dart
+++ lib/practice/keyboard.dart
@@ -16,21 +16,30 @@ class KeyboardScreen extends StatefulWidget {
 }
 
 class _KeyboardScreenState extends State<KeyboardScreen> {
-  String _gameShow(List quest) {
-    return "";
-  }
+  var colorChar = Colors.white;
+  bool notPaused = true;
 
-  _scoreCount(String res) {
+  _scoreCount(String res, context) async {
     if (res != ans) {
+      setState(() {
+        colorChar = Colors.red;
+        _showSnackBar(context);
+      });
+      notPaused = false;
+      await Future.delayed(const Duration(seconds: 2));
       incorrect++;
     } else {
       corrcet++;
     }
 
     if (widget.question.isNotEmpty) {
-      holder = widget.question.removeLast();
-      hira = holder.item1;
-      ans = holder.item2;
+      setState(() {
+        holder = widget.question.removeLast();
+        hira = holder.item1;
+        ans = holder.item2;
+        colorChar = Colors.white;
+        notPaused = true;
+      });
     } else {
       Navigator.of(context).push(
         MaterialPageRoute(
@@ -41,6 +50,21 @@ class _KeyboardScreenState extends State<KeyboardScreen> {
         ),
       );
     }
+  }
+
+  void _showSnackBar(context) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(
+          '$hira -> $ans',
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+        ),
+        duration: const Duration(seconds: 2),
+        behavior: SnackBarBehavior.floating,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+      ),
+    );
   }
 
   // This should probably be put in some kind of initState function instead of using late
@@ -56,52 +80,60 @@ class _KeyboardScreenState extends State<KeyboardScreen> {
     final lettersOnly = FilteringTextInputFormatter.allow(lettersOnlyRegExp);
 
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: backGroundDark,
-        appBar: AppBar(
+      home: Builder(builder: (context) {
+        return Scaffold(
           backgroundColor: backGroundDark,
-          title: const Text("Keyboard"),
-          leading: IconButton(
-            onPressed: () => context.go("/"),
-            icon: const Icon(Icons.arrow_back),
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print(widget.question);
-          },
-          child: const Icon(Icons.start),
-        ),
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Padding(
-            padding: const EdgeInsets.only(bottom: 50.0),
-            child: Text(
-              hira,
-              style: const TextStyle(color: Colors.white, fontSize: 60),
+          appBar: AppBar(
+            backgroundColor: backGroundDark,
+            title: const Text("Keyboard"),
+            leading: IconButton(
+              onPressed: () {
+                if (notPaused) {
+                  context.go("/");
+                }
+              },
+              icon: const Icon(Icons.arrow_back),
             ),
           ),
-          Center(
-            child: SizedBox(
-              width: 200,
-              child: TextField(
-                maxLength: 3,
-                style: const TextStyle(color: Colors.white),
-                decoration: const InputDecoration(
-                  contentPadding:
-                      EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-                  border: OutlineInputBorder(),
-                ),
-                onSubmitted: (value) {
-                  setState(() {
-                    _scoreCount(value);
-                  });
-                },
-                inputFormatters: [lettersOnly],
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              print(widget.question);
+            },
+            child: const Icon(Icons.start),
+          ),
+          body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50.0),
+              child: Text(
+                hira,
+                style: TextStyle(color: colorChar, fontSize: 60),
               ),
             ),
-          ),
-        ]),
-      ),
+            Center(
+              child: SizedBox(
+                width: 200,
+                child: TextField(
+                  maxLength: 3,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+                    border: OutlineInputBorder(),
+                  ),
+                  onSubmitted: (value) {
+                    if (notPaused) {
+                      setState(() {
+                        _scoreCount(value, context);
+                      });
+                    }
+                  },
+                  inputFormatters: [lettersOnly],
+                ),
+              ),
+            ),
+          ]),
+        );
+      }),
     );
   }
 }
