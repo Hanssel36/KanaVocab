4dbcface9ec0611754d7eaad055c4cc95448cf4b
cpuntier <cpuntier@gmail.com>
2023-03-25 18:11:35 -0400

    settings start

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,12 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hirikana/assests/colors.dart';
+import '../my_route.dart';
+
+// class SettingsScreen extends StatelessWidget {
+//   const SettingsScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(),
+//     );
+//   }
+// }
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: backGroundDark,
+        appBar: AppBar(
+          backgroundColor: backGroundDark,
+          title: const Text("Settings"),
+          leading: IconButton(
+            onPressed: () => GoRouter.of(context).pushNamed(homeScreen),
+            icon: const Icon(Icons.arrow_back),
+          ),
+        ),
+        body: MuteBox(),
+      ),
     );
   }
 }
+
+class MuteBox extends StatefulWidget {
+  const MuteBox({super.key});
+
+  @override
+  State<MuteBox> createState() => _MuteBox();
+}
+
+class _MuteBox extends State<MuteBox> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    Color getColor(Set<MaterialState> states) {
+      const Set<MaterialState> interactiveStates = <MaterialState>{
+        MaterialState.pressed,
+        MaterialState.hovered,
+        MaterialState.focused,
+      };
+      if (states.any(interactiveStates.contains)) {
+        return Colors.blue;
+      }
+      return Colors.black;
+    }
+
+    return Row(
+      children: [
+        Text("Mute"),
+        Checkbox(
+          checkColor: Colors.white,
+          fillColor: MaterialStateProperty.resolveWith(getColor),
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        )
+      ],
+    );
+  }
+}
+
+class FontDropDown extends StatefulWidget {}
