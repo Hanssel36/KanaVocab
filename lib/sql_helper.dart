8a79f7184ac5d4d40148066d5fa010facd294cec
Hanssel36 <Hanssel36@gmail.com>
2023-04-14 11:42:11 -0400

    Database

---
+++ lib/sql_helper.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:sqflite/sqflite.dart' as sql;
+
+class SQLHelper {
+  static Future<void> createTables(sql.Database database) async {
+    await database.execute("""CREATE TABLE categories(
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        categoryName TEXT,
+        setsName TEXT,
+        createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
+      """);
+  }
+
+  static Future<sql.Database> db() async {
+    return sql.openDatabase(
+      'db.db',
+      version: 1,
+      onCreate: (sql.Database database, int version) async {
+        await createTables(database);
+      },
+    );
+  }
+
+  static Future<int> createItem(String categoryName, List setsName) async {
+    final db = await SQLHelper.db();
+
+    final data = {'categoryName': categoryName, 'description': setsName};
+    final id = await db.insert('categories', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    return id;
+  }
+
+  static Future<List<Map<String, dynamic>>> getItems() async {
+    final db = await SQLHelper.db();
+    return db.query('categories', orderBy: "id");
+  }
+}
